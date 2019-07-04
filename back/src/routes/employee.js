bdde0cd8683162a90fc972bfcf4462b36dd2eaea
agustinpardolp <agustinpardo@hotmail.com>
2019-07-04 17:44:11 -0300

    configuracion de passport

---
+++ back/src/routes/employee.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router
+const Employee = require("../../db/models/");
+var passport = require("../../config/passport");
+
+router.post('/login', passport.authenticate("local"), function(req, res, next) {
+    res.send(req.user)
+})
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.json(req.user);
+});
+
+
+module.exports = 
