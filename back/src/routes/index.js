5cade03e5938d8d6718cf97ae657d4afbab102ab
LucasAguirre1986 <lucas.aguirre1986@gmail.com>
2019-07-04 18:08:31 -0300

    Ultimos cambios

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Router = express.Router();
 
 //ROUTER API'S
-Router.use('/allowance', require('./allowance'))
+Router.use('/allowance', require('../../db/routes/allowance'))
 //Router.use('/employee', require('../models'))
 
 
