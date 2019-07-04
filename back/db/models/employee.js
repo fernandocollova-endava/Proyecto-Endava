ff2da876ae1d0242149091374bea27a72ed6cb46
agustinpardolp <agustinpardo@hotmail.com>
2019-07-04 15:28:46 -0300

    commit db

---
+++ back/db/models/employee.js
@@ -0,0 +1,61 @@
+const sequelize = require ("sequelize");
+const db  = require ("../../db")
+const crypto = require('crypto');
+
+const Employee = db.define("employee",{
+
+    name: {
+        type: Sequelize.STRING,
+        validate: {
+            notEmpty: true,
+        },
+    },
+    surname:{
+        type:sequelize.STRING,
+        allowNull:false
+    },
+    email: {
+        type: Sequelize.STRING,
+        validate: {
+            isEmail: true
+        },
+        unique: true
+    },
+    sector:{
+        type:sequelize.STRING,
+        allowNull:false
+    }, 
+    isAdmin: {
+        type: Sequelize.BOOLEAN,
+    },
+    password: {
+        type: Sequelize.STRING,
+        validate: {
+            notEmpty: true,
+        }
+    },
+    salt: {
+        type: Sequelize.STRING,
+    }
+})
+
+Employee.generateSalt = function() {
+    return crypto.randomBytes(20).toString('hex')
+}
+
+Employee.prototype.encryptPassword = function(password) {
+    return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+};
+
+Employee.prototype.validatePassword = function(password) {
+    const hash = crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+
+    return this.password === hash;
+};
+
+Employee.addHook('beforeCreate', (employee) => {
+    employee.salt = Employee.generateSalt()
+    employee.password = employee.encryptPassword(employee.password)
+})
+
+module.exports = Employee;
