e05062e56bdc978ea231e06f78206a14f315a12d
Fernando Collova <Fernando.Collova@endava.com>
2019-08-12 17:44:37 -0300

    Test to interpolate postgres pass

--- back/db/index.js
+++ back/db/index.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('proyecto_endava:${process.env.ENDAVA_PROJ_DB_PASS}://db:5432/proyecto_endava', {
+const endava_proj_db_pass = process.env.ENDAVA_PROJ_DB_PASS
+
+const db = new Sequelize('proyecto_endava:${endava_proj_db_pass}://db:5432/proyecto_endava', {
     logging: false
 })
 
