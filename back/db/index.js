b4745ab7c9ae24b3c7394a7e716801e3d27fd3e5
Fernando Collova <Fernando.Collova@endava.com>
2019-08-12 17:03:15 -0300

    Get db pwd from env

--- back/db/index.js
+++ back/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('postgres://localhost:5432/endavaDb', {
+const db = new Sequelize('postgres:${process.env.ENDAVA_PROJ_DB_PASS}://db:5432/endavaDb', {
     logging: false
 })
 
